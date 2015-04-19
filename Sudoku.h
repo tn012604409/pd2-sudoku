df8c7b08ab186f4147b51c7586d10b479874ce72
tn012604409 <f74036116@mail.ncku.edu.tw>
2015-04-19 14:02:47 +0800

    final

--- Sudoku.h
+++ Sudoku.h
@@ -26,25 +26,3 @@ private:
 };
 
 #endif // SUDOKU_H_INCLUDED
-
-
-//Header guard
-/*#ifndef SUDOKU_H_INCLUDED
-#define SUDOKU_H_INCLUDED
-
-class Sudoku
-{
-public:
-    void GiveQuestion();
-    void ReadIn();
-    int Solve();
-    void Print();
-private:
-    char m_board[12][12];
-
-    bool FindBlank(int &row, int &col) const;
-    short GetPossibleNumber(int row, int col) const;
-};
-
-#endif // SUDOKU_H_INCLUDED*/
-
--- main.cpp
+++ main.cpp
@@ -15,16 +15,3 @@ int main(int argc, char *argv[])
 
 
 
-/*#include <iostream>
-#include "Sudoku.h"
-
-using namespace std;
-
-int main(int argc, char *argv[])
-{
-    Sudoku mySudoku;
-    mySudoku.GiveQuestion();
-    mySudoku.ReadIn();
-    mySudoku.Solve();
-}*/
-
